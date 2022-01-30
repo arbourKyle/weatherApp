6ba65f56645b98df5d78d15c095fd8c36204cdf0
KyleA <arbourkyle@gmail.com>
2022-01-29 22:50:58 -0500

    testing first function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+var cityInput = $("#cityInput");
+var cityList = $("#cityList");
+
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
 var apiKey = "b8149456ebf22679f89374a350ed5a0d";
 var city;
 
-fetch(queryURL)
+// fetch(queryURL)
+
+var searchCity = function (event) {
+    event.preventDefault();
+  
+    var user = cityInput.value.trim();
+  
+    if (!user) {
+        
+        localStorage.setItem('cityName', user.value)
+       
+    } else {
+      alert('Please enter a city');
+    }
+  };
+
+  document.addEventListener("submit", searchCity);
+
+
+
