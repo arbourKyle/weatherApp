253e074353d25b390afde9fdc002d81716c98fbb
KyleA <arbourkyle@gmail.com>
2022-02-01 19:31:18 -0500

    I dont know

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,34 @@
-  const key = '980b8738932c5c557956e464ec4e4651';
-
+  const key = 'a36715b7289d49ee08223b849dbb2c64';
+  
   var cityInput = document.getElementById("userInput");
+  var cityHistoryList = document.getElementById("cityList");
   var city = document.getElementById('location');
   var temp = document.getElementById('temp');
   var desc = document.getElementById('desc');
   var button = document.getElementById('submitButton');
 
-  button.addEventListener('click', function() {
+  button.addEventListener('click', function(event) {
+    event.preventDefault();
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityInput.value+'&appid='+key.value)
-    .then(function(response) { return response.json() }) // Convert data to json
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityInput.value+'&appid='+key)
+    .then(function(response) { return response.json() }) 
     .then(function(data) {
       console.log(data)
     })
     .catch(function() {
-      // catch any errors
     })
   })
   
+  
+  cityArr = [];
+  temp = cityInput.value;
+  console.log(temp);
+  
+  function store() {
+    for(var i = 0;i<temp.length; i++);
+    cityArr.push[i];
+    console.log(store(i))
+    
+  }
+  
+  store();
