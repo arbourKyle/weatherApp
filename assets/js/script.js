1c032b014f092dfc54129aedf6c297025ac069b1
KyleA <arbourkyle@gmail.com>
2022-02-03 10:35:07 -0500

    data not defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
   const key = 'a36715b7289d49ee08223b849dbb2c64';
   
-  var cityInput = document.getElementById("userInput");
-  var cityHistoryList = document.getElementById("cityContainer");
-  var city = document.getElementById('location');
-  // dsd
-  var desc = document.getElementById('desc');
-  var button = document.getElementById('submitButton');
+
+  // let today = moment(DD, MMM, do)
+  let cityInput = document.getElementById("userInput");
+  let cityHistoryList = document.getElementById("cityContainer");
+  
+  let cityEl = document.getElementById('location');
+  let tempEl = document.getElementById('temp');
+  let uvEl = document.getElementById('uv');
+  let humidEl = document.getElementById('humid');
+  let windEl = document.getElementById('wind');
+
+
+
+
+  let button = document.getElementById('submitButton');
   
   
   button.addEventListener('click', function geoC(event) {
@@ -15,8 +24,10 @@
     .then(function(response) { return response.json() }) 
     .then(function(data) {
       console.log(data)
+      
       sessionStorage.setItem('lat', data[0].lat)
       sessionStorage.setItem('lon', data[0].lon)
+      
       weather();
     })
     .catch(function() {
@@ -29,11 +40,11 @@
   let lon = sessionStorage.getItem('lon');
   console.log(lat, lon);
 
-  fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&current.uvi'+'&appid='+key)
+  fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&appid='+key)
   .then(function(response) { return response.json() }) 
   .then(function(data) {
     console.log(data.current)
-    console.log(data.current.humidity)
+    sessionStorage.setItem('weather', JSON.stringify(data))
   })
   .catch(function() {
   })
@@ -48,15 +59,49 @@
     console.log(cityArr);
     localStorage.setItem('city', JSON.stringify(cityArr));
     
-    cityName = cityArr[0];
+    popCities();
+    
+  } //end of store func
+  
+  function popCities() {
+    var cityArr = JSON.parse(localStorage.getItem("city"))
+  
+  //create and append city names to boxes
 
+    newArr = cityArr[cityArr.length -1];
+    
     let displayC = document.createElement('div');
     displayC.style = 'border: 1px solid black; margin: 0.5rem 0 0.5rem 0;';
-    let textC = document.createTextNode(cityName);
+    let textC = document.createTextNode(newArr);
 
     displayC.appendChild(textC);
     cityHistoryList.appendChild(displayC);
+
+  //convert appended boxes to clickable items
+    cityHistoryList.addEventListener('click', function () {
+
+    
+
+
+
+}) //End of list click func
+
+populateWeather();
   
-  } //end of store func
+  } //end of popCities func
 
+  function populateWeather() {
+    
+    let temp = sessionStorage.getItem('weather', );
+    // let wind = sessionStorage.getItem('weather', JSON.parse(data.current.wind_speed));
+    // let humid = sessionStorage.getItem('weather', JSON.parse(data.current.humidity));
+    // let uv = sessionStorage.getItem('weather', JSON.parse(data.current.uvi));
 
+    cityEl.textContent = cityInput.value;
+    console.log(cityEl);
+
+    tempEl.textContent = temp.value;
+    windEl.textContent = wind.value;
+    humidEl.textContent = humid.value;
+    uvEl.textContent = uv.value;
+  }
