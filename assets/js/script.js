b732e4169961425bc4bddeb92ba1072b5acb46fa
KyleA <arbourkyle@gmail.com>
2022-02-09 17:18:04 -0500

    still trying to display things without them breaking code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,26 @@
 const key = 'a36715b7289d49ee08223b849dbb2c64';
   
 
-// let today = moment(DD, MMM, do)
+
 let cityInput = document.getElementById("userInput");
 let cityHistoryList = document.getElementById("cityContainer");
 
-let cityEl = document.getElementById('location');
+//id the today forecast card
+let cityEl = document.querySelector('.location');
 let tempEl = document.getElementById('temp');
 let uvEl = document.getElementById('uv');
 let humidEl = document.getElementById('humid');
 let windEl = document.getElementById('wind');
 let dateEl = document.getElementById('today');
-// let iconEl = document.getElementById('icon');
 
+//id the 5 day forecast cards
+let c1 = document.getElementById('c1');
+let c2 = document.getElementById('c2');
+let c3 = document.getElementById('c3');
+let c4 = document.getElementById('c4');
+let c5 = document.getElementById('c5');
+
+//global vars
 var lat;
 var lon;
 var temp;
@@ -22,18 +30,14 @@ var uvi;
 var icon;
 var icon2;
 var data;
-var newName;
-
-"2022-02-09T00:00:51.722Z"
-"2022-02-09T00:00:51.722Z"
+var formatName;
 
 
-
-
-
+//click the submit button and fetch the lat and long
 document.getElementById('submitButton').addEventListener('click', function (event) {
   event.preventDefault()
-  storeCityname(); //store the name given on click
+  formatCityNames(); //format the name given to capitalize the first letter
+  // storeCityName(); //store the name given on click
   
   
   fetch('http://api.openweathermap.org/geo/1.0/direct?q='+cityInput.value+'&appid='+key)
@@ -55,9 +59,9 @@ function weather(data) {
   fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&current.weather.icon&units=metric&exclude=minutely&exclude=hourly&appid='+key)
   .then(function(response) { return response.json() }) 
   .then(function(data) {
-    // console.log(data)
-    todayCard(data);    //calling the display weather function after data is retrieved
-    forecast(data)          //calling forecast function
+    console.log(data)
+    todayCard(data, name)  //calling the display weather function after data is retrieved
+    forecast(data, name)              //calling forecast function
   })
   .catch(function() {
   })
@@ -65,69 +69,70 @@ function weather(data) {
 
 
 // capitalize the first letter of a given name
-function formatCityNames (formatName) {
-  formatName = cityInput.value.toLowerCase().split(' ');
-  
-  for (var i = 0; i < formatName.length; i++) {
-    formatName[i] = formatName[i][0].toUpperCase() + formatName[i].substr(1);
+function formatCityNames () {
+  let name = cityInput.value.toLowerCase().split(' ');
+  for (var i = 0; i < name.length; i++) {
+    name[i] = name[i][0].toUpperCase() + name[i].substr(1);
   }
-  return formatName.join(' ');
+  name = name.join(' ');
+  console.log(name)
+  storeCityName(name)
 } //end of formatting name func
 
 
-function storeCityname(formatName) {
+function storeCityName(name) {
   
   var cityArr = JSON.parse(localStorage.getItem("city")) || [];
-  let tempArr = formatName;
+  let tempArr = name;
   cityArr.push(tempArr);
   
 //the city name is set here
-localStorage.setItem('city', JSON.stringify(cityArr));
+  localStorage.setItem('city', JSON.stringify(cityArr));
+
+  
+
 } //end of storing city name func
 
 
 //display the name of city in a today card, a history list, and a five day card
 
-function todayCard (data, formatName) {
+function todayCard (data, name) {
   let icon = (data.current.weather[0].icon);
   let iconEl = document.createElement('img');
   let iconUrl =  `http://openweathermap.org/img/wn/${icon}.png`;
-  
-  dateEl.textContent = new Date();
-  cityEl.textContent = formatName;
+  // console.log(iconUrl)
+  console.log(name)
+  cityEl.innerText = name;
   tempEl.textContent = data.current.temp;
   windEl.textContent = data.current.wind_speed;
   humidEl.textContent = data.current.humidity;
   uvEl.textContent = data.current.uvi;
   iconEl.setAttribute('src', iconUrl);
-  document.getElementById('location').append(iconEl)
+  document.querySelector('.location').append(iconEl);
+  dateEl.textContent = new Date();
 }
 
+//the history list below the search bar
 
-//converting time to be used for forecast, and making the 5 day forecast
+/* function cityHistory(name) {
+  console.log(name)
+  let listItem = document.createElement('div');
+  listItem.setAttribute('name', name);
+  cityHistoryList.append(listItem);
 
-function forecast(data) {
-  
-  //unicorn time
-    // console.log(Date.now());
-  //regular time
-    // console.log(new Date());
-  //convert unix to regular
-    const uni = Date.now();
-      // const milli = uni * 1000;
-        // console.log(milli);
-            const dateObj = new Date(uni * 1000);
-              console.log(new Date())
-  //convert converted time to 24 hour units
-
-  //set the correct timezone
-  let timeZoneName = data.timezone;
-  console.log(timeZoneName);
-    const dateFormat = dateObj.toLocaleString("en-US", {timeZoneName: "short"})
-      console.log(dateFormat);
+} */
 
 
+//the 5 day forecast
+
+function forecast(data, name) {
+  console.log(data.daily)
+
+  c1.textContent = data.daily[1];
+  c2.textContent = data.daily[2];
+  c3.textContent = data.daily[3];
+  c4.textContent = data.daily[4];
+  c5.textContent = data.daily[5];
 
 
-
-}
+} 
